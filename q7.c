3b620be564a39359300189ff40621b3bf925bdb7
Sneh Kumar Bhagat <112163770+snehbhagat@users.noreply.github.com>
2023-05-01 21:29:57 +0530

    Update q7.c

--- q7.c
+++ q7.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
-    int original_marks[25], revised_marks[25], birth_months[25], i;
-    float original_marks_avg = 0, revised_marks_avg = 0;
-    printf("Enter the original marks and birth month (as a number from 1 to 12) for %d students:\n", &CLASS_SIZE);
-    for (i = 0; i < 25; i++) {
-        printf("Student %d: ", i+1);
-        scanf("%d %d", &original_marks[i], &birth_months[i]);
+    char str[100];
+    int len, freq[26] = {0}, i, rep_index = -1, non_rep_index = -1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+    len = strlen(str);
+    printf("Length of the string is: %d\n", len);
+    for (i = 0; i < len; i++) {
+        freq[str[i] - 'A']++;
     }
-    for (i = 0; i < 25; i++) {
-        revised_marks[i] = original_marks[i] + birth_months[i];
-        original_marks_avg += original_marks[i];
-        revised_marks_avg += revised_marks[i];
+    printf("Word frequency is: %d\n", len);
+    for (i = 0; i < len; i++) {
+        if (freq[str[i] - 'A'] == 1) {
+            non_rep_index = i;
+            break;
+        } else if (freq[str[i] - 'A'] > 1 && rep_index == -1) {
+            rep_index = i;
+        }
     }
-    original_marks_avg /= 25;
-    revised_marks_avg /= 25;
-    if (revised_marks_avg - original_marks_avg >= 5) {
-        printf("Can implement - Significant increase in class average\n");
+    if (rep_index == -1) {
+        printf("No repeated characters found in the string.\n");
     } else {
-        printf("Need not implement - No significant increase in class average\n");
+        printf("First repeated character is: %c\n", str[rep_index]);
+    }
+
+    if (non_rep_index == -1) {
+        printf("No non-repeated characters found in the string.\n");
+    } else {
+        printf("First non-repeated character is: %c\n", str[non_rep_index]);
     }
 
     return 0;
